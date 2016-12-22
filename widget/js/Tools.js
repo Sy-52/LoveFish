08f28793e69ead8cf5ad561704b3901ce49a4d89
Yikai Fan <434411453@qq.com>
2016-12-22 16:54:32 +0800

    简单测了一下发现IE8都有window.requestAnimationFrame后，删除了之前写的兼容函数

--- widget/js/Tools.js
+++ widget/js/Tools.js
@@ -1,14 +1,4 @@
 define(function(){
-	/* 请求动画帧函数兼容函数 */
-	window.requestAnimFrame = (function(){
-		return window.requestAnimationFrame ||
-			   window.webkitRequestAnimationFrame ||
-			   window.mozRequestAnimationFrame  ||
-			   function(callback){
-			   		window.setTimeout(callback,1000/60);
-			   }
-	})();
-
 	/* 渐进函数 -- lerpDistance */
 	function lerpDistance(aim,cur,ratio){
 		return aim + (cur - aim) * ratio;
